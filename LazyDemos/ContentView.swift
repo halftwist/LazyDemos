f4d058fa261360f7cb856823f597998472e0470d
John Kearon <jkearon@hotmail.com>
2025-04-18 09:14:14 -0400

    Swift lesson 59 in progress

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVStack {  // A view that arranges its children in a line that grows vertically, creating items only as needed.
+                ForEach(0..<100) { i in
+                    let _ = print("Dang \(i)")
+                    RedAndCyanView()
+                }
+                
+            }
+           
         }
         .padding()
+    }
+}
+
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack {  // A view that overlays its subviews, aligning them in both axes.
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
