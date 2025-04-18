eb78c8f0af2432d4fda27d0ec63715c44db7bd5d
John Kearon <jkearon@hotmail.com>
2025-04-18 08:51:49 -0400

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by John Kearon on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
